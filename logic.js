9a206f91ffc681955fe65c1c785b14dd688d543b
Chad Carnahan <chadcarnahan1@yahoo.com>
2021-08-19 14:53:26 -0500

    first push

---
+++ logic.js
@@ -0,0 +1,5 @@
+const randomInt = () => {
+  return Math.floor(Math.random() * 9999);
+};
+
+module.exports = randomInt;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const dns = require("dns");
+const randomInt = require("./logic");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +10,30 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlPost = app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  const urlObject = new URL(url);
+  dns.lookup(urlObject.hostname, (err) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      const short_url = randomInt();
+      res.json({ original_url: url, short_url: short_url });
+      app.get(`/api/shorturl/${short_url}`, (req, res) => {
+        console.log(url);
+        res.redirect(url);
+      });
+    }
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
